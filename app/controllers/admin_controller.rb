4725de8df31f08cae547ca3cbaebe0bb99219c80
Tayyab064 <muhammad.tayyabmukhtar@yahoo.com>
2016-03-29 11:33:02 +0500

    admin panel

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,6 @@ class AdminController < ApplicationController
 	before_filter :is_admin?
 	def index
 		
-		@homes = Home.near(params[:locat], 20).order( 'created_at DESC' )
+		@homes = Home.all.order( 'created_at DESC' )
 	end
 end
